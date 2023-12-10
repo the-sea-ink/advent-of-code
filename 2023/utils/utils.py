5b2c3fd20f051418c9c7405bd5af07a4b7f402c5
Xenia <xenia.melman@gmail.com>
2023-12-10 16:50:44 +0100

    putting import reading into a separate file

---
+++ 2023/utils/utils.py
@@ -0,0 +1,6 @@
+def read_input(day: str ='01', filename: str = "input.txt"): 
+    filepath = "2023/inputs/day" + day + "/" + filename 
+    with open(filepath, "r") as f: 
+        vals = f.readlines() 
+        vals = [val.strip("\n") for val in vals] 
+        return vals 
