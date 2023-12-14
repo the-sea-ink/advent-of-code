26c077626d167e58e49a0a6f69b01419ad6bf638
Xenia <xenia.melman@gmail.com>
2023-12-14 17:43:20 +0100

    giving a possibility to pass an integer as a day

--- 2023/utils/utils.py
+++ 2023/utils/utils.py
@@ -1,5 +1,5 @@
 def read_input(day: str ='01', filename: str = "input.txt"): 
-    filepath = "2023/inputs/day" + day + "/" + filename 
+    filepath = "2023/inputs/day" + str(day) + "/" + filename 
     with open(filepath, "r") as f: 
         vals = f.readlines() 
         vals = [val.strip("\n") for val in vals] 
