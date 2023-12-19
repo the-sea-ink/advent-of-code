4f2610b8fef8539a45e427e6f74f20a9deff574c
Xenia <xenia.melman@gmail.com>
2023-12-19 07:26:34 +0100

    part 2 in progress

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,18 +1,25 @@
+import re
 from utils.utils import read_input
+
+def hash(code):
+    curr_val = 0
+    for symbol in code:
+        curr_val += ord(symbol)
+        curr_val *= 17
+        curr_val = curr_val%256
+    return curr_val
 
 def part1(vals):
     sum = 0
     separated_vals = vals[0].split(',')
     for code in separated_vals:
-        curr_val = 0
-        for symbol in code:
-            curr_val += ord(symbol)
-            curr_val *= 17
-            curr_val = curr_val%256
-        sum += curr_val
+        sum += hash(code)
     return sum
 
 def part2(vals):
+    separated_vals = vals[0].split(',')
+    #for code in separated_vals:
+    
     return
 
 if __name__ == "__main__":
